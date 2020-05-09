eda3960ac2b72d63f8184d555e0d24a0e3910b4b
MatanNoach <61862949+MatanNoach@users.noreply.github.com>
2020-05-09 23:26:49 +0300

    trying to create a fork

--- src/ass4/src/TestPart1.java
+++ src/ass4/src/TestPart1.java
@@ -14,6 +14,5 @@ public class TestPart1 {
         map.put(v2.getVar(), (double) 50);
         System.out.println(e.evaluate(map));
         System.out.println(e2.evaluate(map));
-        //adding some changes to check
     }
 }
